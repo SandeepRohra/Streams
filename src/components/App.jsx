bc36c896fa35056c19c34011c9897ca8724240f6
sandeep rohra <sandeep12rohra@gmail.com>
2023-01-23 15:40:15 +0530

    Doubt

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter,Route,Link } from "react-router-dom";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StrimList";
+import StreamCreate from "./streams/streamCreate";
+import Header from "./Header";
+const App =()=>{
+    return(
+        <div className="ui container">
+            <BrowserRouter>
+            <div>
+            <Header />
+                <Route path="/" exact component={StreamList}></Route>
+                <Route path="/streams/new" exact component={StreamCreate}></Route>
+                <Route path="/streams/edit" exact component={StreamEdit}></Route>
+                <Route path="/streams/delete" exact component={StreamDelete}></Route>
+                <Route path="/streams/show" exact component={StreamShow}></Route>
+            </div>
+            </BrowserRouter>
+        </div>
+    )
+}
+export default App
